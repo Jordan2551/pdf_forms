f59059b2cce59c2efeaabbfcaae22436dcf56fbb
jordan2551 <jordan1158@gmail.com>
2018-03-23 22:08:31 -0400

    work on step 2

--- app/assets/javascripts/client_applications/step_2.js
+++ app/assets/javascripts/client_applications/step_2.js
@@ -1,8 +1,13 @@
-
 $(document).ready(function() {
+
   $("#client_alimony_child_support_required").on("click", function(){
-      //possible issue: dynamic js doesn't bind events to generated js
-      alert("j");
-      $("#alimony_fields").toggle(this.checked);
+    if(this.checked){
+      $("#alimony_fields").show();
+      $("#cannot_assist").hide();
+    }
+    else{
+      $("#alimony_fields").hide();
+      $("#cannot_assist").show();
+    }
   })
 });

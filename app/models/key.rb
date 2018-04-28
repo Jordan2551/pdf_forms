63ee4a35ee8d5f5f4b8f767c5c1ccb01f2fddd5e
jordan2551 <jordan1158@gmail.com>
2018-04-28 09:26:46 -0400

    working on front end

--- app/models/key.rb
+++
@@ -1,6 +0,0 @@
-class Key < ApplicationRecord
-  attr_accessor :ap_account_id
-  attr_accessor :ap_key
-  attr_encrypted :ap_account_id, key: 'zjpgsNBZQRW-snzsT-1F_w'
-  attr_encrypted :ap_key, key: 'b97Mv-8xT7iVKdOiYx_ggAOyb6NCmugp63EVUr4EKFonLr2g7WADrwM0Z3wPD89J'
-end

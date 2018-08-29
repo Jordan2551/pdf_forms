3ecb1e913a436a1f2960e62bd694ef26d8b119b8
jordan2551 <jordan1158@gmail.com>
2018-08-29 17:55:16 -0400

    added step 1 mailing functionality

--- app/mailers/register_mailer.rb
+++ app/mailers/register_mailer.rb
@@ -1,13 +1,12 @@
 class RegisterMailer < ApplicationMailer
 
   default from: 'slclientinfo@gmail.com'
-  email_david = "david@owedsupport.com"
   email_lawrence = "lawrence@owedsupport.com"
-  email_jordan = "jordan1158@gmail.com"
 
-  def register_step_1_email(client)
+  def register_step_1_email(client, recepient)
     @client = client
-    mail(to: "jordan1158@gmail.com", subject: 'New Support Solutions Client Application')
+    mail(to: recepient, subject: 'New Support Solutions Client Application')
+
   end
 
 end
